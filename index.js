7118712961a2810c914fa3ecd46d8d9bafe13de7
Max Harker <maxharker@mail.weber.edu>
2026-05-03 22:13:15 -0600

    Updated cors origin

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const io = new Server(server, {
     cors: {
         origin: [
             'http://localhost:5173',
-            'https://chess-react-max.netlify.app/'
+            'https://chess.maxharker.com'
         ]
     }
 })
